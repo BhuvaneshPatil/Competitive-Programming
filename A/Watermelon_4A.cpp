f0944f55f47904c859dc0d388960cf70477ec072
BhuvaneshPatil <bhuvaneshpatil80642@gmail.com>
2020-10-04 08:47:53 +0530

    Watermelon_4A.cpp added in A

---
+++ A/Watermelon_4A.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int weight;
+    cin>>weight;
+    if (weight>2){
+           if(weight%2==0){
+        cout<<"yes";
+    }else{
+        cout<<"no";
+    } 
+    }else{
+        cout<<"no";
+    }
+ 
+    return 0;
+}
